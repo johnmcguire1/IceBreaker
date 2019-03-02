2fd8076cb59e2a073ac2c09fa9c268b3acad18bd
OctoCactus <alexmamina@gmail.com>
2019-03-02 20:00:52 +0000

    Added constructor for Details_Input class

--- app/src/main/java/com/example/icebreaker/Details_Input.java
+++ app/src/main/java/com/example/icebreaker/Details_Input.java
@@ -1,7 +1,9 @@
 package com.example.icebreaker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import java.lang.String;
+
 
 public class Details_Input extends AppCompatActivity {
 
@@ -11,5 +13,17 @@ public class Details_Input extends AppCompatActivity {
         setContentView(R.layout.activity_details__input);
     }
 
+
+    String name, uni, colour, editor, jaffa, tabsspaces, mode;
+
+    public Details_Input() {
+        this.name = findViewById(R.id.name).toString();
+        uni = findViewById(R.id.university).toString();
+        this.colour = findViewById(R.id.colour).toString();
+        editor = findViewById(R.id.texteditor).toString();
+        this.jaffa = findViewById(R.id.jaffacake).toString();
+        this.tabsspaces = findViewById(R.id.tabsspacestext).toString();
+        this.mode = findViewById(R.id.darklighttext).toString();
+        }
 
 }
