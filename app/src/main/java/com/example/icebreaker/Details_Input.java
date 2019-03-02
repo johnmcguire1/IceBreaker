fb4924653abe4a1db49555fe5f7447387064936b
Carina Fiedler <carina.x.fiedler@gmail.com>
2019-03-02 19:57:56 +0000

    generateMethod

--- app/src/main/java/com/example/icebreaker/Details_Input.java
+++ app/src/main/java/com/example/icebreaker/Details_Input.java
@@ -2,6 +2,7 @@ package com.example.icebreaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Details_Input extends AppCompatActivity {
 
@@ -10,6 +11,11 @@ public class Details_Input extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details__input);
     }
-
+    String name, uni, colour, editor, jaffa, tabspaces, mode;
+    public String generateText(){
+        return "My name is " + name + ". " + "I go to " + uni + ". " + "My favourite colour is "
+                + colour + ". " + "My favourite editor is " + editor + ". " + "A Jaffa Cake is definately a "
+                + jaffa + ". " + "I always use " + tabspaces + ". " + "I prefer " + mode + ".";
+}
 
 }
