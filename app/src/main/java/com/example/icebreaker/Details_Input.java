2fdc255116ba064aa640371c94a070f50c9815b5
OctoCactus <alexmamina@gmail.com>
2019-03-03 00:16:35 +0000

    Added validation and stupid options for names etc.

--- app/src/main/java/com/example/icebreaker/Details_Input.java
+++ app/src/main/java/com/example/icebreaker/Details_Input.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
 import java.lang.String;
+import java.util.Objects;
 
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,7 @@ import android.widget.Toast;
 
 public class Details_Input extends AppCompatActivity {
 
-    EditText name, uni, colour, editor;
+    EditText name, uni, colour, editor, age, siblings;
     RadioButton cake, biscuit, tabs, spaces, dark, light;
     Button save;
 
@@ -25,9 +26,11 @@ public class Details_Input extends AppCompatActivity {
 
         //EditText:
         name = findViewById(R.id.name);
+        age = findViewById(R.id.age);
         uni = findViewById(R.id.university);
         colour = findViewById(R.id.colour);
         editor = findViewById(R.id.texteditor);
+        siblings = findViewById(R.id.siblings);
 
         //RadioButtons:
         cake = findViewById(R.id.cake);
@@ -40,25 +43,55 @@ public class Details_Input extends AppCompatActivity {
     }
 
     public void generateText(View view){
-        String nameValue = name.getText().toString();
-        String uniValue = uni.getText().toString();
-        String colourValue = colour.getText().toString();
-        String editorValue = editor.getText().toString();
+        String nameValue = "Ice Man Nice Man";
+        String uniValue = "Antarctican University of Political Science";
+        int ageValue = 80;
+        String colourValue = "Pink";
+        String editorValue = "Microsoft Word";
+        String sibling = "";
 
-        Boolean cakeValue = cake.isSelected();
+        if (!name.getText().toString().matches("")) {
+                    nameValue = name.getText().toString();}
+        if (!uni.getText().toString().matches("")) {
+                    uniValue = uni.getText().toString(); }
+        if (!siblings.getText().toString().matches("")) {
+            sibling = uni.getText().toString(); }
+        if (!age.getText().toString().matches("")) {
+            ageValue = Integer.parseInt(age.getText().toString()); }
+        if (!colour.getText().toString().matches("")) {
+            colourValue = colour.getText().toString(); }
+        if (!editor.getText().toString().matches("")) {
+            editorValue = editor.getText().toString(); }
+
+        //Boolean cakeValue = cake.isSelected();
         Boolean biscuitValue = biscuit.isSelected();
-
+        Boolean tabsValue = tabs.isSelected();
+       // Boolean spacesValue = spaces.isSelected();
+        Boolean darkValue = dark.isSelected();
+       // Boolean lightValue = light.isSelected();
         String jaffa = "";
+        String tabsspaces = "";
+        String mode = "";
 
-        if(cakeValue)
-            jaffa = "cake";
-        else if(biscuitValue)
+        if(biscuitValue)
             jaffa = "biscuit";
+        else
+            jaffa = "cake";
 
-        String output = "My name is " + nameValue + ". " + "I go to " + uniValue + ". " + "My favourite colour is "
-                 + colourValue + ". " + "My favourite editor is " + editorValue + ". " + "A Jaffa Cake is definately a "
-                + jaffa + "";
-                //". " + "I always use " + tabsspaces + ". " + "I prefer " + mode + ".";
+        if(tabsValue)
+            tabsspaces = "tabs";
+        else
+            tabsspaces = "spaces";
+
+        if(darkValue)
+            mode = "dark";
+        else
+            mode = "light";
+
+
+        String output = "My name is " + nameValue + ", I am " + ageValue + " years old. I go to " + uniValue + ". " + "My favourite colour is "
+                 + colourValue + ". " + "My favourite editor is " + editorValue + ". " + "A Jaffa Cake is definitely a "
+                + jaffa + "" + ". " + "I always use " + tabsspaces + ". " + "I prefer " + mode + "mode. I have " + sibling + " siblings.";
 
         Toast.makeText(Details_Input.this, output, Toast.LENGTH_LONG).show();
     }
